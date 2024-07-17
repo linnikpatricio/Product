f1ce20cc8cdcb602d5ab250acda495a169385ce0
linnikpatricio <linnikpatricio17@gmail.com>
2024-07-17 20:29:00 -0300

    product

---
+++ src/main/java/org/example/application/Main.java
@@ -0,0 +1,46 @@
+package org.example.application;
+
+import org.example.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter Product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name,price);
+        String novoNome = "Eletrônico";
+        product.setName(novoNome);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProdutcs(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+    }
+}
